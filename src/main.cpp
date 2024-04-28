618780905c8b1ea14ea7338fcc9dd2a36590d30d
marllaest022075 <marllaest2075@hotmail.com>
2024-04-28 07:19:41 -0700

    Blink buildin led

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,32 @@
 #include <Arduino.h>
 
+const int pin = 13;
+const long interval = 500;
+
 // put function declarations here:
+void TurnOnOffPin();
 
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(pin, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  TurnOnOffPin();
 }
 
 // put function definitions here:
+void TurnOnOffPin()
+{
+  static long previousMillis = 0;
+  long currentMillis = millis();
+  if (currentMillis - previousMillis > interval)
+  {
+    digitalWrite(pin, !digitalRead(pin) );
+    previousMillis = currentMillis;
+  }
+}
